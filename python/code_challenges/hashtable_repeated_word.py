3aa374cffeeabed524ecb5526105e05386ba7f27
Mike Shen <michael.m.shen@gmail.com>
2023-05-01 19:09:30 +0000

    added files

--- python/code_challenges/hashtable_repeated_word.py
+++ python/code_challenges/hashtable_repeated_word.py
@@ -2,27 +2,33 @@ from data_structures.hashtable import Hashtable
 import re
 
 def first_repeated_word(phrase):
+    # variables
     word_list = phrase.split()
     hashtable = Hashtable()
 
+    # check empty or 1 word phrase
     if len(word_list) <= 1:
         return None
 
     for index, word in enumerate(word_list):
-        # clean word list to lower case, without ending punctuation
-        word_list[index] = re.sub(r'[^\w\s]+$','', word).strip()
-        word_list[index] = word_list[index].lower()
+        word_list[index] = clean(word)
 
         # Check to see if it's in hashtable
-        # if hashtable.has(word_list[index]):
         if word_list[index] in hashtable.keys():
             return word_list[index]
 
         else:
+            # Add to hashtable
             hashtable.set(word_list[index], word_list[index])
 
     # No match
     return None
+
+def clean(input_word):
+    # clean word list to lower case, without ending punctuation
+    output_word = re.sub(r'[^\w\s]+$','', input_word).strip()
+    output_word = output_word.lower()
+    return output_word
 
 if __name__ == "__main__":
     phrase = "banana !ap!ple! APPLE banana"
